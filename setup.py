a105323a296fc750997f5edf0ef05ff3f5e2b7f1
Alex Kessinger <alex@mixedmedialabs.com>
2013-12-20 23:01:46 -0800

    Getting ready for release

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
+requirements = list(open('dist_requirements.txt').readlines())
+
 setup(name="lark",
       version='0.0.1',
       description="Lark is a RESTy interface for python",
@@ -11,6 +13,6 @@ setup(name="lark",
       author_email="voidfiles@gmail.com",
       url="http://github.com/voidfiles/lark",
       packages=find_packages(exclude=['tests']),
-      install_requires=open('requirements.txt').read().split('\n'),
-      keywords="rest lark json,
+      install_requires=requirements,
+      keywords="rest lark json",
       zip_safe=True)
