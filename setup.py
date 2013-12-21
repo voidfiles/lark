8f4d4cfd79161e24bfe1d71c0b34703bbe0acc36
Alex Kessinger <alex@mixedmedialabs.com>
2013-12-20 23:26:27 -0800

    Got the setup.py close

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
-requirements = list(open('dist_requirements.txt').readlines())
-
 setup(name="lark",
-      version='0.0.1',
+      version='0.0.4',
       description="Lark is a RESTy interface for python",
       long_description=open('README.rst').read(),
       license="MIT",
@@ -13,6 +11,11 @@ setup(name="lark",
       author_email="voidfiles@gmail.com",
       url="http://github.com/voidfiles/lark",
       packages=find_packages(exclude=['tests']),
-      install_requires=requirements,
+      install_requires=[
+          'colander==0.9.9',
+          'iso8601==0.1.8',
+          'redis==2.8.0',
+          'Flask==0.10.1',
+      ],
       keywords="rest lark json",
       zip_safe=True)
